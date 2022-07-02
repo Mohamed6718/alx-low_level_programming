f7a4e56e32d6ecc3f05d5a4ee3aae1c673f8d93b
Mohamed6718 <iammoaziiz@gmail.com>
2022-07-02 03:00:16 -0700

    added a new file

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,23 +13,20 @@ int main(void)
  	while (e <58)
 	{
 		i = 48;
-				while (i < 58)
-							{
-											if (e != i && e < i)
-															{
-																				putchar(e);
-																								putchar(i);
-																												if (i == 57 && e == 56)
-																																	{
-																																							break;
-																																											}
-																																putchar(',');
-																																				putchar(' ');
-																																							}
-														i++;
-																}
-						e++;
-							}
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{																		putchar(e);
+																					putchar(i);
+																					if (i == 57 && e == 56)
+																					{																																			break;
+																					}
+																					putchar(',');																																	putchar(' ');
+																				} 
+			i++;
+		}
+		e++;
+	}
 		putchar('\n');
 			return (0);
 }
